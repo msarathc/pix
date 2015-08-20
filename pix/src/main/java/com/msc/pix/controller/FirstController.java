df52580328ce2faed71afc49009ebc8707a9bd09
msarathc <sarath.chandar@gmail.com>
2015-08-19 20:27:00 -0500

    Added controller and jackson binding

--- pix/src/main/java/com/msc/pix/controller/FirstController.java
+++ pix/src/main/java/com/msc/pix/controller/FirstController.java
@@ -1,8 +1,34 @@
 package com.msc.pix.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msc.pix.response.WelcomeResponse;
+
 @RestController
+@RequestMapping("/home")
 public class FirstController {
+
+	@RequestMapping(value="welcome", method=RequestMethod.GET)
+	public String welcome(@RequestParam(value="who") String who){
+		return "Welcome, "+ who + "!";
+	}
+	@RequestMapping(value="hello/{who}", method=RequestMethod.GET)
+	public String hello(@PathVariable String who){
+		return "Hello, "+ who + "!";
+	}
+
+	@RequestMapping(value="sayHello", method=RequestMethod.GET)
+	public @ResponseBody WelcomeResponse sayHello(){
+		WelcomeResponse welcomeResponse = new WelcomeResponse();
+		welcomeResponse.setName("Sarath");
+		welcomeResponse.setPhone("123456");
+		return welcomeResponse;
+	}
+
 
 }
---
+++ pix/src/main/java/com/msc/pix/response/WelcomeResponse.java
@@ -0,0 +1,56 @@
+package com.msc.pix.response;
+
+public class WelcomeResponse {
+
+	public String name;
+	public String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WelcomeResponse other = (WelcomeResponse) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "WelcomeResponse [name=" + name + ", phone=" + phone + "]";
+	}
+	
+	
+	
+	
+}
